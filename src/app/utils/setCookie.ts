21e2b73ed842007a8d739a817ba36c245a336cf4
Sohelrana2815 <sohel152302@gmail.com>
2026-09-20 22:26:58 +0600

    dd

--- src/app/utils/setCookie.ts
+++ src/app/utils/setCookie.ts
@@ -10,7 +10,7 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
     res.cookie("accessToken", tokenInfo.accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 60 * 60 * 60 * 1000, // 1 day
     });
   }
@@ -19,7 +19,7 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
     res.cookie("refreshToken", tokenInfo.refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 30 * 60 * 60 * 60 * 1000, // 30 days
     });
   }
